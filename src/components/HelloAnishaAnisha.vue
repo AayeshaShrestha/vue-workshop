81f47f634592327bf8e4421f3374abecc3fcc715
Aayesha Shrestha <aayesha.shrestha.5688@gmail.com>
2019-09-28 11:28:55 +0545

    hello world

---
+++ src/components/HelloAnishaAnisha.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>Hello Anisha jsfnajsb jas ans</div>
+</template>
+
+<script>
+export default {
+  name: "HelloAnishaAnisha"
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    Hello World
+    <p>{{message}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Life is beautiful"
+    };
+  },
+  name: "HelloWorld"
+};
+</script>
+
+<style>
+</style>
